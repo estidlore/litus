85f4055ec3c666ce22cce6e3b88e6ca9c544a23e
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-03 01:01:57 -0500

    feat: add math mean & median
    - Add math.mean
    - Add math.median

--- src/math/index.test.ts
+++ src/math/index.test.ts
@@ -9,6 +9,19 @@ describe("math", () => {
     expect(math.add(arr, 5)).toStrictEqual(exp);
   });
 
+  it("mean", () => {
+    expect.assertions(1);
+
+    expect(math.mean([-4, 1, 0, 3, 2])).toBe(2 / 5);
+  });
+
+  it("median", () => {
+    expect.assertions(2);
+
+    expect(math.median([-4, 1, 0, 3, 2])).toBe(1);
+    expect(math.median([1, 0, 3, 2])).toBe(1.5);
+  });
+
   it("mod", () => {
     const scalars = [-2, -1, 0, 1, 2];
     expect.assertions(scalars.length * 3);
--- src/math/index.ts
+++ src/math/index.ts
@@ -2,6 +2,17 @@ const add = (arr: number[], n: number): number[] => {
   return arr.map((el) => el + n);
 };
 
+const mean = (arr: number[]): number => {
+  return sum(arr) / arr.length;
+};
+
+const median = (arr: number[]): number => {
+  const mid = arr.length / 2;
+  const a = Math.floor(mid - 0.5);
+  const b = Math.ceil(mid + 0.5);
+  return mean([...arr].sort().slice(a, b));
+};
+
 const mod = (x: number, m: number): number => {
   return ((x % m) + m) % m;
 };
@@ -10,4 +21,4 @@ const sum = (arr: number[]): number => {
   return arr.reduce((acc, el) => acc + el, 0);
 };
 
-export { add, mod, sum };
+export { add, mean, median, mod, sum };
