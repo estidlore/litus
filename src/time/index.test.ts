987095ce11829026373db38abf125a573a253e3f
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-05 17:42:13 -0500

    feat: add time .add & .dayTime
    - Add time.add to sum given time
    - Add time.dayTime to get time ignoring days
    - Round ms in time.to to avoid losing precision

--- src/time/index.test.ts
+++ src/time/index.test.ts
@@ -1,6 +1,15 @@
 import * as time from ".";
 
 describe("time", () => {
+  it("add", () => {
+    expect.assertions(2);
+
+    const t1 = time.time(1, "m");
+    const t2 = time.time(18, "s");
+    expect(time.add(t1, t2)).toStrictEqual({ unit: "m", val: 1.3 });
+    expect(time.add(t2, t1)).toStrictEqual({ unit: "s", val: 78 });
+  });
+
   it("date", () => {
     expect.assertions(2);
 
@@ -8,6 +17,14 @@ describe("time", () => {
     const t2 = time.time(2.5, "s");
     expect(time.date(t1).getTime()).toBe(t1.val);
     expect(time.date(t2).getTime()).toBe(t1.val);
+  });
+
+  it("dayTime", () => {
+    expect.assertions(2);
+
+    const d = time.date(time.add(time.time(48, "h"), time.time(2.1, "s")));
+    expect(time.dayTime(d)).toStrictEqual({ unit: "ms", val: 2100 });
+    expect(time.dayTime(d, "s")).toStrictEqual({ unit: "s", val: 2.1 });
   });
 
   it("get", () => {
--- src/time/index.ts
+++ src/time/index.ts
@@ -8,8 +8,18 @@ const MS: Record<TimeUnit, number> = {
   s: 1e3
 };
 
+const add = (t: Time, t2: Time): Time => {
+  return time(t.val + to(t2, t.unit).val, t.unit);
+};
+
 const date = (t: Time): Date => {
   return new Date(to(t).val);
+};
+
+const dayTime = (d: Date, unit: TimeUnit = "ms"): Time => {
+  const t = get(d, "d");
+  t.val %= 1;
+  return to(t, unit);
 };
 
 const get = (d: Date, unit: TimeUnit = "ms"): Time => {
@@ -21,7 +31,8 @@ const time = (val: number, unit: TimeUnit = "ms"): Time => {
 };
 
 const to = (t: Time, unit: TimeUnit = "ms"): Time => {
-  return time((MS[t.unit] / MS[unit]) * t.val, unit);
+  const ms = Math.round(MS[t.unit] * t.val);
+  return time(ms / MS[unit], unit);
 };
 
-export { date, get, MS, time, to };
+export { add, date, dayTime, get, MS, time, to };
