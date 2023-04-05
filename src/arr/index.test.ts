2030f114950115172b02912821b9c38e8cb082c7
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-04-05 00:53:16 -0500

    feat: add arr fill, group & groupBy
    - Add arr.fill to create arr filled with a value
    - Add arr.group to group arr of objs by custom getKey callback
    - Add arr.groupBy to group arr of objs by a propKey

--- src/arr/index.test.ts
+++ src/arr/index.test.ts
@@ -1,6 +1,57 @@
 import * as arr from ".";
 
+interface Ball {
+  colors: string[];
+  name: string;
+  size: "big" | "small";
+}
+
+const balls: Ball[] = [
+  {
+    colors: ["black", "white"],
+    name: "Baseball",
+    size: "small"
+  },
+  {
+    colors: ["green", "red"],
+    name: "Beach",
+    size: "big"
+  },
+  {
+    colors: ["black", "orange"],
+    name: "Basketball",
+    size: "big"
+  }
+];
+
 describe("arr", () => {
+  it("fill", () => {
+    expect.assertions(2);
+
+    expect(arr.fill(3, -1)).toStrictEqual([-1, -1, -1]);
+    expect(arr.fill(2, "a")).toStrictEqual(["a", "a"]);
+  });
+
+  it("group", () => {
+    expect.assertions(1);
+
+    const res = arr.group(balls, ({ size }) => size);
+    expect(res).toStrictEqual({
+      big: [balls[1], balls[2]],
+      small: [balls[0]]
+    });
+  });
+
+  it("groupBy", () => {
+    expect.assertions(1);
+
+    const res = arr.groupBy(balls, "size");
+    expect(res).toStrictEqual({
+      big: [balls[1], balls[2]],
+      small: [balls[0]]
+    });
+  });
+
   it("range", () => {
     expect.assertions(4);
 
--- src/arr/index.ts
+++ src/arr/index.ts
@@ -1,3 +1,33 @@
+const fill = <T>(n: number, val: T): T[] => {
+  const res = Array(n);
+  for (let i = 0; i < n; i++) {
+    res[i] = val;
+  }
+  return res;
+};
+
+const group = <T extends object, K extends PropertyKey>(
+  arr: T[],
+  getKey: (obj: T) => K
+): Record<K, T[]> => {
+  const res = <Record<K, T[]>>{};
+  arr.forEach((el) => {
+    const val = getKey(el);
+    if (res[val] === undefined) {
+      res[val] = [];
+    }
+    res[val].push(el);
+  });
+  return res;
+};
+
+const groupBy = <T extends object, K extends keyof T>(
+  arr: T[],
+  key: T[K] extends PropertyKey ? K : never
+): Record<PropertyKey & T[K], T[]> => {
+  return group(arr, (obj) => <PropertyKey & T[K]>obj[key]);
+};
+
 const range = (a: number, b: number, step = 1): number[] => {
   const n = Math.ceil((b - a) / step);
   if (n <= 0) {
@@ -11,4 +41,4 @@ const range = (a: number, b: number, step = 1): number[] => {
   return res;
 };
 
-export { range };
+export { fill, group, groupBy, range };
