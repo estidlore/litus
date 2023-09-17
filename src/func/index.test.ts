a37ce588e5ff4e4159c7da1370b0c66b32e0c0af
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2023-09-16 20:30:07 -0500

    feat: export func.debounce
    - Export debounce in func
    - Add func.debounce test

--- src/func/index.test.ts
+++ src/func/index.test.ts
@@ -21,4 +21,22 @@ describe("func", () => {
     expect(curried("Bye", 3)(true)).toBe("Bye.3-T");
     expect(curried("Hi", 4, false)).toBe("Hi.4-F");
   });
+
+  it("debounce", () => {
+    expect.assertions(2);
+    jest.useFakeTimers();
+
+    const fn = jest.fn();
+    const debouncedFn = func.debounce(fn);
+    debouncedFn();
+    jest.advanceTimersByTime(10);
+    debouncedFn();
+
+    jest.advanceTimersByTime(90);
+    expect(fn).not.toBeCalled();
+    jest.advanceTimersByTime(10);
+    expect(fn).toBeCalledTimes(1);
+
+    jest.useRealTimers();
+  });
 });
