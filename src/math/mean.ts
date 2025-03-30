8a89b96e40cfc784e7c49ec58713c145c426dcbc
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2025-03-30 17:32:26 -0500

    feat: allow mean weights

--- src/math/mean.ts
+++ src/math/mean.ts
@@ -1,5 +1,11 @@
 import { sum } from "./sum";
 
-export const mean = (arr: number[]): number => {
-  return sum(arr) / arr.length;
+export const mean = (arr: number[], weights?: number[]): number => {
+  if (weights === undefined) {
+    return sum(arr) / arr.length;
+  }
+  if (arr.length !== weights.length) {
+    throw new Error("Nums and weights must have the same length");
+  }
+  return sum(arr.map((el, i) => el * weights[i])) / sum(weights);
 };
