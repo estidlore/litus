983cc11f884bed931782d1691f95e06d477c28fc
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2025-04-27 00:54:31 -0500

    feat: improve obj utils
    - Improve obj.isObj & obj.has return types
    - Make obj entries, keys & vals only well-typed aliases of respective Object funcs

--- src/obj/keys.ts
+++ src/obj/keys.ts
@@ -1,5 +1,3 @@
 import type { Key } from "./types";
 
-export const keys = <T extends object>(obj: T): Key<T>[] => {
-  return Object.keys(obj) as Key<T>[];
-};
+export const keys: <T extends object>(obj: T) => Key<T>[] = Object.keys;
