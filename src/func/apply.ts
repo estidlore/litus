3a1e130493b4cea09440d64b468ce31f3528cea0
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2025-06-09 23:21:47 -0500

    replace this with undefined in fn.apply calls

--- src/func/apply.ts
+++ src/func/apply.ts
@@ -1,5 +1,5 @@
 export const apply = <T extends unknown[], R>(
   fn: (...args: T) => R
 ): ((arg: T) => R) => {
-  return (arg: T): R => fn.apply(this, arg);
+  return (arg: T): R => fn.apply(undefined, arg);
 };
--- src/func/curry.ts
+++ src/func/curry.ts
@@ -7,10 +7,10 @@ export const curry = <R, T extends unknown[]>(
   return <A extends CurryArgs<T>>(...args: A): CurryFnRes<R, T, A> => {
     const missing = arity - args.length;
     if (missing <= 0) {
-      return fn.apply(this, args as unknown as T) as CurryFnRes<R, T, A>;
+      return fn.apply(undefined, args as unknown as T) as CurryFnRes<R, T, A>;
     }
     return curry(<Rest extends CurryRest<T, A>>(...rArgs: Rest): R => {
-      return fn.apply(this, (args as unknown[]).concat(rArgs) as T);
+      return fn.apply(undefined, (args as unknown[]).concat(rArgs) as T);
     }, missing) as CurryFnRes<R, T, A>;
   };
 };
