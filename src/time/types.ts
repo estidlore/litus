5b6c0186ff25c020fefb30c581dd1510fa61c580
Estid Felipe Lozano Reyes <estidlore@outlook.com>
2026-03-01 00:01:45 -0500

    rem time

--- src/time/types.ts
+++
@@ -1,5 +0,0 @@
-type TimeFormat = "date" | "full" | "time";
-type TimeFormatOptions = Record<TimeFormat, Intl.DateTimeFormatOptions>;
-type TimeUnit = "d" | "h" | "m" | "ms" | "s";
-
-export type { TimeFormat, TimeFormatOptions, TimeUnit };
